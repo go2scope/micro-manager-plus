5c8bb19569b02965b853670d579c3d7a9c981260
Lazar Rankovic <lazar@go2scope.com>
2019-08-06 15:30:19 +0200

    dataio.G2SDatasetReaderS3: implemented image_pixels method

--- python/dataio/g2sdataset/g2sdataset_s3.py
+++ python/dataio/g2sdataset/g2sdataset_s3.py
@@ -1,10 +1,13 @@
 import json
 from json import JSONDecodeError
+import cv2
+import numpy as np
 
 from dataio.g2sdataset import G2SDatasetReader
 from dataio.g2sdataset.g2sdataset import G2SPosDatasetReader
 from dataio.g2sdataset.g2sdataset import SummaryMeta
 from dataio.g2sdataset.g2sdataset import G2SDataError
+from dataio.g2sdataset.g2sdataset import ImageMeta
 from awslp.awslp import S3Bucket
 
 
@@ -56,10 +59,10 @@ class G2SDatasetReaderS3(G2SDatasetReader):
         list_of_dirs = [e.replace(self._path + '/', '').split('/')[0] for e in list_of_dirs]
         list_of_dirs = set(list_of_dirs)
         # TODO check allowed extensions
-        list_of_dirs = [e for e in list_of_dirs if not e.endswith('.txt') or
-                                                       e.endswith('.png') or
-                                                       e.endswith('.tif') or
-                                                       e.endswith('.jpeg') or e == '']
+        list_of_dirs = [e for e in list_of_dirs if not (e.endswith('.txt') or
+                                                        e.endswith('.png') or
+                                                        e.endswith('.tif') or
+                                                        e.endswith('.jpeg') or e == '')]
         self._positions = [None] * len(list_of_dirs)
         for pos_dir in list_of_dirs:
             pds = G2SPosDatasetReaderS3(self._path + '/' + pos_dir, self._s3_bucket)
@@ -80,10 +83,10 @@ class G2SPosDatasetReaderS3(G2SPosDatasetReader):
         self._s3_bucket = s3_bucket
         super().__init__(path)
 
-    def _load_meta(self)  -> None:
+    def _load_meta(self) -> None:
         """ Loads the entire data set, including images
         """
-        md_str = self._s3_bucket.get_file_content(self._path + '/' +  G2SPosDatasetReader.METADATA_FILE_NAME)
+        md_str = self._s3_bucket.get_file_content(self._path + '/' + G2SPosDatasetReader.METADATA_FILE_NAME)
         # there is a strange bug in some of the micro-manager datasets where closing "}" is
         # missing, so we try to fix
         try:
@@ -103,3 +106,33 @@ class G2SPosDatasetReaderS3(G2SPosDatasetReader):
         self._height = summary[SummaryMeta.HEIGHT]
         self._pixel_type = summary[SummaryMeta.PIXEL_TYPE]
         self._bit_depth = summary[SummaryMeta.BIT_DEPTH]
+
+    def image_pixels(self, channel_index: int=0, channel_name: str="", z_index: int=0, t_index: int=0) -> np.array:
+        """ Get image pixels received from S3.
+        :param channel_index:
+        :param channel_name:
+        :param z_index:
+        :param t_index:
+        :return: image pixels matrix
+        """
+        ch_index = self._get_channel_index(channel_index, channel_name)
+        if ch_index not in range(len(self._channel_names)) or z_index not in range(self._z_slices) or \
+                t_index not in range(0, self._frames):
+            raise G2SDataError("Invalid image coordinates: channel=%d, slice=%d, frame=%d" % (ch_index, z_index, t_index))
+
+        image_path = self._path + '/' + self._metadata[G2SPosDatasetReader.get_frame_key(channel_index, z_index, t_index)][ImageMeta.FILE_NAME]
+        data = self._s3_bucket.get_file_object(image_path)
+        image_pixel_type = self.pixel_type()
+
+        # TODO support missing types
+        pixel_type = dict()
+        pixel_type['GRAY8'] = np.uint8
+        pixel_type['GRAY16'] = np.uint16
+        pixel_type['RGB16'] = np.uint8
+        pixel_type['RGB32'] = np.uint16
+
+        file_stream = np.fromstring(data, pixel_type[image_pixel_type])
+        cv2_image = cv2.imdecode(file_stream, cv2.IMREAD_UNCHANGED)
+        if cv2_image is None:
+            raise G2SDataError("Invalid image reference: " + image_path)
+        return cv2_image
--- python/dataio/test/test_dataset_read_s3.py
+++ python/dataio/test/test_dataset_read_s3.py
@@ -8,14 +8,10 @@ cognito = Cognito(application_id=AWS_CONFIGURATION['application_id'],
                   identity_pool_id=AWS_CONFIGURATION['identity_pool_id'],
                   region_name=AWS_CONFIGURATION['region_name'])
 
-username = 'lazar'
-password = '123qwe'
-image_dir = 'images'
-image_name = 'img_000000000_Violet_012.tif'
-dataset_name = 'lazar/NIH3T3_1'
-
-identity_id, access_key_id, secret_key, session_token = cognito.get_aws_credentials(username=username,
-                                                                                    password=password)
+dataset_name = 'nenad/Run_1'
+# dataset_name = 'lazar/NIH3T3_1'
+identity_id, access_key_id, secret_key, session_token = cognito.get_aws_credentials(username=AWS_CONFIGURATION['username'],
+                                                                                    password=AWS_CONFIGURATION['password'])
 
 dr_s3 = G2SDatasetReaderS3(dataset_name,
                            AWS_CONFIGURATION['s3_bucket_name'],
@@ -26,6 +22,13 @@ dr_s3 = G2SDatasetReaderS3(dataset_name,
 
 print('Dataset name   : ', dr_s3.name())
 print('Num of channels: ', dr_s3.num_channels())
+print('Channels       : ', dr_s3.channel_names())
 print('Width          : ', dr_s3.width())
 print('Height         : ', dr_s3.height())
 print('Pixel type     : ', dr_s3.pixel_type())
+print('Frames         : ', dr_s3.num_frames())
+print('Slices         : ', dr_s3.num_z_slices())
+print()
+print(dr_s3.image_pixels(0, 0, 0, 0))
+print()
+print(dr_s3.image_pixels(position_index=0, channel_index=2, z_index=0, t_index=30))
