122c0ede7484f2d96946c220d41eea1d952da018
Nenad Amodaj <nenad@go2scope.com>
2020-04-25 17:56:38 -0700

    updated package meta

--- python/go2scope/setup.py
+++ python/go2scope/setup.py
@@ -5,12 +5,14 @@ with open("go2scope/dataio/README.md", "r") as fh:
 
 setuptools.setup(
     name="go2scope",
-    version="1.0.0",
+    version="0.0.0",
     author="Nenad Amodaj",
     author_email="nenad@go2scope.com",
     description="go2scope support functions for python: data I/O",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
+    url="https://github.com/go2scope/micro-manager-plus/tree/master/python/go2scope",
     install_requires=[
         'numpy==1.16.4', 'opencv-python==4.2.0.34',
     ],
@@ -19,4 +21,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3.7',
 )
